463fb922909baeee44e93d860e472d416218bc53
juanelas <j.hernandez@upc.edu>
2023-04-14 14:33:44 +0200

    old types deleted

--- types/browser-base64.d.cts
+++
@@ -1,3 +0,0 @@
-export declare const base64Encode: (bytes: Uint8Array) => string;
-export declare const base64Decode: (encoded: string) => Uint8Array;
-//# sourceMappingURL=browser-base64.d.ts.map
--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-export type TypedArray = Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | BigInt64Array | BigUint64Array;
-export declare function encode(input: ArrayBuffer | TypedArray | Buffer | string, urlsafe?: boolean, padding?: boolean): string;
-export declare function decode(base64: string, stringOutput?: boolean): Uint8Array | string;
-//# sourceMappingURL=index.d.ts.map
